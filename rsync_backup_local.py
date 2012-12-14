fd3d11fb8007e7fd010bef0c24b5f9cfe7172fb8
jcli <jcjc.li@gmail.com>
2012-12-14 13:59:41 -0800

    updated local sync script

--- rsync_backup_local.py
+++ rsync_backup_local.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 import commands
@@ -6,22 +7,72 @@ import time
 import subprocess
 
 class Backup:
-
-    def __init__(self, src, dst_root, port):
+    def __latest_backup(self):
+        remote_path = self.dst_root
+        # check if folder exist
+        time.sleep(1)
+        output = commands.getoutput('ls ' + remote_path)
+        if (output.find('No such file or directory')!=-1):
+            print 'creating destination root path'
+            time.sleep(1)
+            output = commands.getoutput('mkdir ' + remote_path)
+            return ''
+        elif (output.replace('\n', '').replace(' ', '')==''):
+            print "destination root empty"
+            return ''
+        else:
+            #sort and return the latest backup
+            output = output.split('\n')
+            output.sort(reverse=True)
+            return self.dst_root + output[0]
+            
+    def __init__(self, src, dst_root):
+        if (src[-1]!='/'):
+            src = src+'/';
         self.src = src
-        self.dst_root = dst_root+src.split('/')[-2]+'/'
-        self.port = port
+        if (dst_root[-1]!='/'):
+            dst_root = dst_root + '/';
+        self.dst_root = dst_root+src.split('/')[-2]+'/';
         self.current_date = str(datetime.datetime.utcfromtimestamp(time.time())).replace(' ', '_').replace(':','.')
-        self.latest_backup_path = self.latest_backup()
+        self.latest_backup_path = self.__latest_backup()
         self.log_file_name = '/tmp/' + self.src.split('/')[-2] + '_backup.log'
         
-    def run_backup(self):
-        if (self.__src_outdate()):
-            print self.src, ' out of date!'
-            self.__subprocess_remote_sync()
+    def __create_backup_folder(self):
+        remote_path = self.dst_root
+        if (remote_path[-1]!='/'):
+            remote_path += '/'
+        remote_path += self.current_date
+        time.sleep(1)
+        output = commands.getoutput('mkdir -p ' + remote_path)
+        return remote_path
+
+    def __subprocess_rsync(self):
+        # create the rsync command string
+        if (self.latest_backup_path==''):
+            link_dest = ''
         else:
-            self.target_path = self.__create_backup_folder()
-            self.__subprocess_rsync_to_remote()
+            link_dest = '--link-dest=' + self.latest_backup_path
+        rsync_command = 'rsync -avh --chmod a+rw -e --delete ' + link_dest + ' '\
+                        + self.src + ' ' + self.target_path + ' &> ' + self.log_file_name
+        print '\n' + rsync_command + '\n'
+        commands.getoutput(rsync_command)
+        #append the backup date
+        log_file = open(self.log_file_name, 'a')
+        log_file.write('------------------------')
+        log_file.write('date: ' + self.current_date)
+        log_file.write('------------------------\n')
+        log_file.write(rsync_command)
+        log_file.close()
+        log_move_command = 'cp ' + self.log_file_name + ' ' + self.src + self.log_file_name.split('/')[-1]
+        print '\n' + log_move_command + '\n'
+        commands.getoutput(log_move_command)
+        log_move_command = 'cp ' + self.log_file_name + ' ' + self.target_path + '/' + self.log_file_name.split('/')[-1]
+        print '\n' + log_move_command + '\n'
+        commands.getoutput(log_move_command)
+        
+    def run_backup(self):
+        self.target_path = self.__create_backup_folder()
+        self.__subprocess_rsync()
 
 def parse_config(filename):
     try:
@@ -65,13 +116,13 @@ def main():
     print dst_root
     print src_dirs
     
-    # backup_objects = []
-    # for src_dir in src_dirs:
-    #     backup_object = Backup(src_dir, dst_root)
-    #     backup_objects.append(backup_object)
+    backup_objects = []
+    for src_dir in src_dirs:
+        backup_object = Backup(src_dir, dst_root)
+        backup_objects.append(backup_object)
 
-    # for backup_objest in backup_objects:
-    #     backup_objest.run_backup()
+    for backup_objest in backup_objects:
+        backup_objest.run_backup()
         
 if __name__ == "__main__":
     main()
